5d92ff260c8ffaea9b4f43f4493002ceadeb9f95
GabrielaSodre <gabi.silvasodre@hotmail.com>
2023-09-01 14:49:34 -0300

    Implementação de rodapé em todas as páginas com exceção de login e cadastro, além disso liberando acesso público para as rotas das páginas contidas no rodapé

--- src/main/java/com/soulcode/goserviceapp/config/SecurityConfig.java
+++ src/main/java/com/soulcode/goserviceapp/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final String[] PUBLIC_ROUTES = {"/", "/home", "/auth/**", "/css/**", "/js/**", "/assets/**", "/api/**"};
+    private final String[] PUBLIC_ROUTES = {"/", "/home", "/auth/**", "/css/**", "/js/**", "/assets/**", "/api/**", "/politica-de-privacidade", "/termos-e-condicoes", "/faq"};
 
     @Bean
     public PasswordEncoder passwordEncoder() {
